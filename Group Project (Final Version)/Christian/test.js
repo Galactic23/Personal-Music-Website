304a2c689873ee72c126686011bc712b413bb614
Galactic23 <68677617+Galactic23@users.noreply.github.com>
2021-08-16 23:43:49 -0500

    Add files via upload

---
+++ Group Project (Final Version)/Christian/test.js
@@ -0,0 +1,12 @@
+function start()
+{
+	var x = "five";
+	var statement;
+
+	statement = "six";
+
+	document.getElementById("statement").innerHTML = statement;
+
+}
+
+window.addEventListener("load", start, false);
